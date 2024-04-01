9a69cd02a6f05795fafc82de72e82162c45814e4
Yers <eabuov4@gmail.com>
2024-04-01 20:28:45 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <h1>Weather App</h1>
+    <p>Weather in your city</p>
+    <input type="text" placeholder="City">
+    <button>See weather</button>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #1f0f24;
+  text-align: center;
+  color: #fff;
+}
+.wrapper h1 {
+  margin-top: 50px;
+}
+.wrapper p {
+  margin-top: 20px;
+}
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
+
+</style>
